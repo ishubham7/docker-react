1502267f24c0edff026040157178ea2358b54cd4
shubham sharma <shubham.sharma2@oodles.io>
2023-08-07 21:22:42 +0530

    test

--- src/App.js
+++ src/App.js
@@ -9,14 +9,7 @@ function App() {
         <p>
           Hi there i'm live now great
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
